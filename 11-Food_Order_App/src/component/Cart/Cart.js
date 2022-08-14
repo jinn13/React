df325ae08542284d8d66c95fe071b209d2adfdc2
jinn13 <dydn512@gmail.com>
2022-08-15 08:40:18 +0900

    20220815

---
+++ 11-Food_Order_App/src/component/Cart/Cart.js
@@ -0,0 +1,21 @@
+import classes from "./Cart.module.css";
+
+const Cart = (props) => {
+  const cartItems = (
+    <ul>
+      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.9 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <div>
+      {cartItems}
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Cart;
