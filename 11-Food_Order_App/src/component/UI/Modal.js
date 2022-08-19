cf1306144eefcc6f57423737ccc9134cdc982990
jinn13 <dydn512@gmail.com>
2022-08-19 23:45:44 +0900

    20220819

---
+++ 11-Food_Order_App/src/component/UI/Modal.js
@@ -0,0 +1,17 @@
+import classes from "./Modal.module.css";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const Modal = (props) => {};
+
+export default Modal;
