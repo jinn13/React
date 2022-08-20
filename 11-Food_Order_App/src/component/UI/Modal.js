20c300ff161c37b95b4209a0d2bff54b31049253
jinn13 <dydn512@gmail.com>
2022-08-20 23:57:28 +0900

    20220820

--- 11-Food_Order_App/src/component/UI/Modal.js
+++ 11-Food_Order_App/src/component/UI/Modal.js
@@ -1,3 +1,6 @@
+import { Fragment } from "react";
+import { ReactDOM } from "react-dom";
+
 import classes from "./Modal.module.css";
 
 const Backdrop = (props) => {
@@ -12,6 +15,18 @@ const ModalOverlay = (props) => {
   );
 };
 
-const Modal = (props) => {};
+const portalElement = document.getElementById("overlays");
+
+const Modal = (props) => {
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {
+        (ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>),
+        portalElement)
+      }
+    </Fragment>
+  );
+};
 
 export default Modal;
