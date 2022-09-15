73c768ff82fd8a487333712097c9f24acd130ec3
jinn13 <dydn512@gmail.com>
2022-09-15 23:48:31 +1200

    20220915

---
+++ 12-Behind_React/src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+import './App.css';
+
+function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph(prevShowParagraph => !prevShowParagraph);
+  };
+
+  return (
+    <div className="app">
+      <h1>Hi there!</h1>
+      {showParagraph && <p>This is new!</p>}
+      <button onClick={toggleParagraphHandler}>Toggle Paragraph!</button>
+    </div>
+  );
+}
+
+export default App;
