bfb89ce0eaad65d46d38439d85f539556dda465b
jinn13 <dydn512@gmail.com>
2022-10-04 23:59:04 +1300

    20221004

--- 13-Class_Component/src/components/UserFinder.js
+++ 13-Class_Component/src/components/UserFinder.js
@@ -1,4 +1,4 @@
-import { Fragment, useState, useEffect } from 'react';
+import { Fragment, useState, useEffect, Component } from 'react';
 
 import Users from './Users';
 import classes from './UserFinder.module.css';
@@ -8,6 +8,27 @@ const DUMMY_USERS = [
     { id: 'u2', name: 'Manuel' },
     { id: 'u3', name: 'Julie' },
   ];
+
+class UserFinder extends Component{
+  constructor(){
+    this.state={
+      filteredUsers : DUMMY_USERS, 
+      searchTerm : ''
+    };
+  }
+
+  searchChangeHandler(){
+    this.setState({searchTerm : event.target.value});
+  }
+  render() {
+    return (
+      <Fragment>
+        <input type='search' onChange={this.searchChangeHandler.bind(this)} />
+        <Users users={state.filteredUsers} />
+      </Fragment>
+    );
+  }
+};
 
 const UserFinder = () => {
   const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
