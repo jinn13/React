32e5ed348fcd9583b309387a5a5c4aa8b5161e9e
jinn13 <dydn512@gmail.com>
2022-10-06 23:50:49 +1300

    20221006

--- 13-Class_Component/src/App.js
+++ 13-Class_Component/src/App.js
@@ -1,11 +1,23 @@
 import UserFinder from './components/UserFinder';
+import UsersContext from './store/users-context';
 // import Users from './components/Users';
 
+const DUMMY_USERS = [
+  { id: 'u1', name: 'Max' },
+  { id: 'u2', name: 'Manuel' },
+  { id: 'u3', name: 'Julie' },
+];
+
+
 function App() {
+  const usersContext = {
+    users: DUMMY_USERS
+  };
+
   return (
-    <div>
+    <UsersContext.Provider value={usersContext}>
       <UserFinder />
-    </div>
+    </UsersContext.Provider>
   );
 }
 
