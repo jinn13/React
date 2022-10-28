d4f3cbb0de9146367681e7d7f202b01d2b5e3f8e
jinn13 <75795444+jinn13@users.noreply.github.com>
2022-10-28 23:59:34 +1300

    20221028

--- 14-HTTP_Request/src/App.js
+++ 14-HTTP_Request/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState, useCallback} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -8,7 +8,11 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  async function fetchMoviesHandler() {
+  useEffect(()=>{
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  const fetchMoviesHandler = useCallback(async() => {
     setIsLoading(true);
     setError(null);
     try {
@@ -35,7 +39,7 @@ function App() {
       }
       setIsLoading(false);
     
-  };
+  }, []);
 
   let content = <p>Found no movies.</p>
   
